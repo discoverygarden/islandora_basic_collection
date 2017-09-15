73c88c572bd13a48b583ee0bcb4a17d06797c2f2
willtp87 <willtp87@hotmail.com>
2017-09-15 18:52:58 -0300

    some dep handling

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,6 +8,8 @@ namespace Drupal\islandora_basic_collection\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Form\FormBuilderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use AbstractObject;
 
@@ -15,6 +17,29 @@ use AbstractObject;
  * Default controller for the islandora_basic_collection module.
  */
 class DefaultController extends ControllerBase {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(FormBuilderInterface $form_builder) {
+    $this->formBuilder = $form_builder;
+  }
 
   public function islandora_basic_collection_manage_access($object = NULL, Drupal\Core\Session\AccountInterface $account) {
     $collection_models = islandora_basic_collection_get_collection_content_models();
@@ -77,7 +102,7 @@ EOQ;
   public function islandora_basic_collection_ingest_action(AbstractObject $object) {
     if (($configuration = islandora_basic_collection_get_ingest_configuration($object)) !== FALSE) {
       module_load_include('inc', 'islandora', 'includes/ingest.form');
-      return \Drupal::formBuilder()->getForm('Drupal\islandora\Form\IslandoraIngestForm', $configuration);
+      return $this->formBuilder->getForm('Drupal\islandora\Form\IslandoraIngestForm', $configuration);
     }
     drupal_not_found();
   }
